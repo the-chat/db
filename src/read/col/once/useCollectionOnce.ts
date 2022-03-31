4c0bcac7a734a343db6d52660204a6a9d956603a
the-chat <thechatllc@gmail.com>
2022-03-31 14:14:37 +0500

    useCollectionOnce

---
+++ src/read/col/once/useCollectionOnce.ts
@@ -0,0 +1,17 @@
+import { Firestore, Query, QuerySnapshot } from "firebase/firestore"
+import { useCollectionOnce as useFirebaseCollectionOnce } from "react-firebase-hooks/firestore"
+import { OnceOptions } from "react-firebase-hooks/firestore/dist/firestore/types"
+import { RCFH, Obj } from "../../../types"
+import useCollectionBase from "../base"
+
+export const useCollectionOnce = <T extends Obj>(
+  db: Firestore,
+  pathOrQuery: string | Query,
+  opts: OnceOptions
+) =>
+  useCollectionBase<QuerySnapshot | undefined, OnceOptions>(
+    db,
+    useFirebaseCollectionOnce,
+    pathOrQuery,
+    opts
+  ) as RCFH<QuerySnapshot<T>>
