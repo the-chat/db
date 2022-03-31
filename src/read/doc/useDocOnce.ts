e586bfad71ed01faa93a0cdaa9f765aa66b543c3
the-chat <thechatllc@gmail.com>
2022-03-31 15:42:43 +0500

    useDocData

---
+++ src/read/doc/useDocOnce.ts
@@ -0,0 +1,24 @@
+import { FirestoreError, Firestore } from "@firebase/firestore"
+import { useDocumentData as useFirebaseDocumentData } from "react-firebase-hooks/firestore"
+import {
+  Data,
+  Options,
+} from "react-firebase-hooks/firestore/dist/firestore/types"
+import { RDFH } from "../../types"
+import { useDocBase } from "./base"
+
+export const useDocData = <T>(
+  db: Firestore,
+  path: string,
+  defV?: T,
+  opts?: Options
+) => {
+  const [val, loading, error] = useDocBase<Data | undefined, Options>(
+    db,
+    useFirebaseDocumentData,
+    path,
+    opts
+  )
+
+  return [val || defV, loading, error] as RDFH<Data<T> | undefined>
+}
