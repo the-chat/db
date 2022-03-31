fd7c58141ef33e9c8f5b0b2c6fdb53b3631b39a3
the-chat <thechatllc@gmail.com>
2022-03-31 15:43:43 +0500

    doc types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,9 @@
-import { FirestoreError, Query, CollectionReference } from "@firebase/firestore"
+import {
+  FirestoreError,
+  Query,
+  CollectionReference,
+  DocumentReference,
+} from "@firebase/firestore"
 
 // todo?: to types pkg
 export type Obj = Record<string, unknown>
@@ -8,3 +13,5 @@ export type FH<Ret, Ref, Opts> = (
 ) => [Ret, boolean, FirestoreError | undefined] // Firebase Hook
 export type CFH<Ret, Opts> = FH<Ret, Query | CollectionReference, Opts> // Collection Firebase Hook
 export type RCFH<Ret> = ReturnType<CFH<Ret, never>> // Return of Collection Firebase Hook
+export type DFH<Ret, Opts> = FH<Ret, DocumentReference | undefined, Opts> // Document Firebase Hook
+export type RDFH<Ret> = ReturnType<DFH<Ret, never>> // Return Document Firebase Hook
