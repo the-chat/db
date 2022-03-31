e49a8d1d36717aa634ecd6544ff651f1b02c4a5a
the-chat <thechatllc@gmail.com>
2022-03-31 14:16:40 +0500

    col index

---
+++ src/read/col/index.ts
@@ -0,0 +1,3 @@
+export { useCollectionOnce, useCollectionDataOnce } from "./once"
+export { readColOnce, readColDataOnce } from "./plainFns"
+export { useCollection, useCollectionData } from "./withHandlingChanges"
