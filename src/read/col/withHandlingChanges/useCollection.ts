85b20ffbc33ec3daebe0e3cd0621ad792b39c014
the-chat <thechatllc@gmail.com>
2022-03-31 14:15:03 +0500

    useCollection

---
+++ src/read/col/withHandlingChanges/useCollection.ts
@@ -0,0 +1,17 @@
+import { Firestore, Query, QuerySnapshot } from "firebase/firestore"
+import { Options } from "react-firebase-hooks/firestore/dist/firestore/types"
+import { RCFH, Obj } from "../../../types"
+import useCollectionBase from "../base"
+import { useCollection as useFirebaseCollection } from "react-firebase-hooks/firestore"
+
+export const useCollection = <T extends Obj>(
+  db: Firestore,
+  pathOrQuery: string | Query,
+  opts?: Options
+) =>
+  useCollectionBase<QuerySnapshot | undefined, Options>(
+    db,
+    useFirebaseCollection,
+    pathOrQuery,
+    opts
+  ) as RCFH<QuerySnapshot<T>>
